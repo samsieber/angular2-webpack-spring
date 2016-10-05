3c2a031f76873bdf42a24213183a9f7afd6bec33
Andre Hartmann <andrehartmann@gmx.de>
2016-10-05 22:12:43 +0200

    version upgrade + fix tests and imports

--- src/main/webui/app/app.component.spec.ts
+++ src/main/webui/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import {TestBed} from "@angular/core/testing";
 import {RouterTestingModule} from "@angular/router/testing";
 import {AppComponent} from "./app.component";
+import {UserService} from "./user.service";
+import "rxjs/add/observable/of";
 
 describe('App', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [AppComponent],
-            imports: [RouterTestingModule.withRoutes([])]
+            imports: [RouterTestingModule.withRoutes([])],
+            providers: [{provide: UserService, useValue: {}}]
         });
     });
     it('should work', () => {
--- src/main/webui/app/user.service.ts
+++ src/main/webui/app/user.service.ts
@@ -2,6 +2,9 @@ import {Injectable} from "@angular/core";
 import {Http, Response, Headers} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import "rxjs/add/observable/of";
 
 @Injectable()
 export class UserService {
